09ebb611ff663eec979a09831ccf228be88423d5
HiluLagos <128090306+HiluLagos@users.noreply.github.com>
2024-10-03 17:05:58 -0300

    Fixed controller

--- src/main/kotlin/austral/ingsis/parser/controller/CommunicationController.kt
+++ src/main/kotlin/austral/ingsis/parser/controller/CommunicationController.kt
@@ -3,11 +3,13 @@ package austral.ingsis.parser.controller
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.RestClient
+
 // import org.springframework.web.client.RestClient
 
 @RestController
 class CommunicationController {
-//    val testClient: RestClient = RestClient.builder().baseUrl("http://localhost:8080").build()
+    val testClient: RestClient = RestClient.builder().baseUrl("http://localhost:8080").build()
 //
 // //    @GetMapping("/testRequestMessage")
 // //    fun callEndpoint(): ResponseEntity<String> {
