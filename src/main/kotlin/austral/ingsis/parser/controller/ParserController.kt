f431a1f0e5d36eda835bc4e687a9e9aa4b565d0c
Tomibernardez04 <tbernardez@mail.austral.edu.ar>
2024-10-12 13:56:59 -0300

    All working, missing coverage (5_%)

--- src/main/kotlin/austral/ingsis/parser/controller/ParserController.kt
+++ src/main/kotlin/austral/ingsis/parser/controller/ParserController.kt
@@ -1,23 +1,24 @@
 package austral.ingsis.parser.controller
 
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.http.ResponseEntity
 import runner.Operations
 import java.io.ByteArrayInputStream
 import java.nio.charset.StandardCharsets
 
 data class SnippetRequest(
     val code: String,
-    val language: String
+    val language: String,
 )
 
 @RestController("/parser")
 class ParserController {
-
     @PostMapping("/validate")
-    fun validateSnippet(@RequestBody request: SnippetRequest): ResponseEntity<String> {
+    fun validateSnippet(
+        @RequestBody request: SnippetRequest,
+    ): ResponseEntity<String> {
         val codeInputStream = ByteArrayInputStream(request.code.toByteArray(StandardCharsets.UTF_8))
         val runner = Operations(codeInputStream, "1.1.0", null)
 
