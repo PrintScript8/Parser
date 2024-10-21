f2a383ec501d76adc89cb445727bb03ed6beadb7
ezequielrickert <erickert@mail.austral.edu.ar>
2024-10-21 09:23:29 -0300

    Spotless applied

--- src/main/kotlin/austral/ingsis/parser/message/ParserRequestHandler.kt
+++ src/main/kotlin/austral/ingsis/parser/message/ParserRequestHandler.kt
@@ -1,6 +1,8 @@
 package austral.ingsis.parser.message
 
 import org.austral.ingsis.redis.RedisStreamConsumer
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.data.redis.connection.stream.ObjectRecord
@@ -8,8 +10,6 @@ import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.stream.StreamReceiver
 import org.springframework.stereotype.Component
 import java.time.Duration
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 
 @Component
 class ParserRequestHandler
@@ -19,13 +19,11 @@ class ParserRequestHandler
         @Value("\${stream.key}") streamKey: String,
         @Value("\${groups.product}") groupId: String,
     ) : RedisStreamConsumer<ExecuteRequest>(streamKey, groupId, redis) {
+        private val logger: Logger = LoggerFactory.getLogger(ParserRequestHandler::class.java)
 
-    private val logger: Logger = LoggerFactory.getLogger(ParserRequestHandler::class.java)
-
-
-    override fun onMessage(record: ObjectRecord<String, ExecuteRequest>) {
-        logger.info("Received action: ${record.value.action}")
-    }
+        override fun onMessage(record: ObjectRecord<String, ExecuteRequest>) {
+            logger.info("Received action: ${record.value.action}")
+        }
 
         @Suppress("MagicNumber")
         override fun options(): StreamReceiver.StreamReceiverOptions<String, ObjectRecord<String, ExecuteRequest>> {
